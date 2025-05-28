daf2e7108e443bc51d356fddaa3f33cce3315487
dianaseptak <dianasheptak@gmail.com>
2025-05-28 16:30:58 +0300

    :tada: setup Drizzle Kit DB and Turso

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ export default withNuxt(antfu({
         semi: true,
         quotes: "double",
     },
-    ignores: [".pnpm-store/**", "**/*.yml", "**/*.yaml"],
+    ignores: [".pnpm-store/**", "**/*.yml", "**/*.yaml", "**/migrations/*"],
 }, {
     rules: {
         "vue/max-attributes-per-line": ["error", {
