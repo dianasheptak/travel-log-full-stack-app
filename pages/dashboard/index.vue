22dce80ba9351e4e4ee7f77dbbc7ee7b81fa1439
dianasheptak <103140025+dianasheptak@users.noreply.github.com>
2025-05-30 16:10:39 +0300

    have expanded sidebar btn logic and added tooltip to the menu (#57)

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,5 +1,59 @@
+<script setup lang="ts">
+import SidebarButton from "~/components/sidebar-button.vue";
+
+const isSidebarOpen = ref(true);
+
+function toggleSidebar() {
+    isSidebarOpen.value = !isSidebarOpen.value;
+    localStorage.setItem("isSidebarOpen", isSidebarOpen.value.toString());
+}
+
+onMounted(() => {
+    isSidebarOpen.value = localStorage.getItem("isSidebarOpen") === true;
+});
+</script>
+
 <template>
-    <div>
-        LOOGED INNNN
+    <div class="flex-1 flex">
+        <div class="bg-base-200 transition-all duration-300" :class="{ 'w-72': isSidebarOpen, 'w-16': !isSidebarOpen }">
+            <div
+                class="flex cursor-pointer"
+                :class="{ 'justify-center': !isSidebarOpen, 'justify-end': isSidebarOpen }"
+                @click="toggleSidebar"
+            >
+                <Icon
+                    v-if="isSidebarOpen"
+                    name="tabler:chevron-left"
+                    size="36"
+                />
+                <Icon
+                    v-else
+                    name="tabler:chevron-right"
+                    size="36"
+                />
+            </div>
+            <div class="flex flex-col gap-3">
+                <SidebarButton
+                    icon="tabler:map"
+                    title="Locations"
+                    href="/dashboard"
+                    :show-label="isSidebarOpen"
+                />
+                <SidebarButton
+                    icon="tabler:circle-plus-filled"
+                    title="Add Location"
+                    href="/dashboard/add"
+                    :show-label="isSidebarOpen"
+                />
+
+                <SidebarButton
+                    href="/sign-out"
+                    icon="tabler:logout-2"
+                    title=" Sign Out"
+                    :show-label="isSidebarOpen"
+                />
+            </div>
+        </div>
+        <div class="flex-1" />
     </div>
 </template>
