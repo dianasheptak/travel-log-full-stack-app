eac84ebc03bce87eeea2da5cb5a1acde04a33623
dianaseptak <dianasheptak@gmail.com>
2025-07-06 19:40:04 +0300

    feat: created locationList and location page component with cards

--- lib/types.ts
+++ lib/types.ts
@@ -1,10 +1,6 @@
-// import type { User } from "better-auth"
-//
-// declare module "h3" {
-//     // eslint-disable-next-line ts/consistent-type-definitions
-//     interface H3EventContext {
-//         user?: Omit<User, "id"> & {
-//             id: number;
-//         };
-//     }
-// }
+declare module "h3" {
+    // eslint-disable-next-line ts/consistent-type-definitions
+    interface H3EventContext {
+        user?: UserWithId;
+    }
+}
