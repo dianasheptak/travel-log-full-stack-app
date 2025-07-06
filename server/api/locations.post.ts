1d7f5a92aec01113955a118b0a1435eefc1e344c
dianaseptak <dianasheptak@gmail.com>
2025-07-06 15:28:30 +0300

    feat: validation location slugs for names, locations and ids

--- server/api/locations.post.ts
+++ server/api/locations.post.ts
@@ -1,11 +1,13 @@
 import type { DrizzleError } from "drizzle-orm";
 
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { customAlphabet } from "nanoid";
 import slugify from "slug";
 
 import db from "~/lib/db";
 import { InsertLocation, location } from "~/lib/db/schema";
+
+const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", 5);
 
 export default defineEventHandler(async (event) => {
     const result = await readValidatedBody(event, InsertLocation.safeParse);
@@ -39,19 +41,33 @@ export default defineEventHandler(async (event) => {
         }));
     }
 
-    const slug = slugify(result.data.name);
-    let existing = !!(await db.query.location.findFirst({
-        where: eq(location.slug, slug),
-    }));
+    let slug = slugify(result.data.name);
+    const existingLocation = await db.query.location.findFirst({
+        where:
+        and(
+            eq(location.name, result.data.name),
+            eq(location.userId, event.context.user.id),
+        ),
+    });
+
+    if (existingLocation) {
+        return sendError(event, createError({
+            statusCode: 409,
+            statusMessage: "A location with that name already exists!",
+        }));
+    }
 
     while (existing) {
-        const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", 5);
-
-        const idSlug = `${slug}-${nanoid}`;
+        const id = nanoid();
+        const idSlug = `${slug}-${id}`;
 
         existing = !!(await db.query.location.findFirst({
             where: eq(location.slug, idSlug),
         }));
+
+        if (!existing) {
+            slug = idSlug;
+        }
     }
 
     try {
