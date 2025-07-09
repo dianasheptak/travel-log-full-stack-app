286cb3c19ada08c9722eb4df37f35229678ed318
dianaseptak <dianasheptak@gmail.com>
2025-07-09 15:00:32 +0300

    feat: minor logic and display fixes

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -6,7 +6,6 @@ import { useSidebarStore } from "~/stores/sidebar";
 const isSidebarOpen = ref(true);
 const sidebarStore = useSidebarStore();
 const locationsStore = useLocationsStore();
-
 const route = useRoute();
 
 function toggleSidebar() {
